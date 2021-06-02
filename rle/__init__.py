8df766626567095ad7a30da986d55711efd96034
egorchistov <egorchistov@mail.ru>
2021-06-02 22:38:02 +0300

    rle: up count type from u8 to u32

--- rle/__init__.py
+++ rle/__init__.py
@@ -7,15 +7,30 @@ def rle_code(istream):
     ostream = bytearray()
     for char, same in groupby(istream):
         count = sum(1 for _ in same)
-        ostream.extend((count, char))
+        ostream.extend(int2bytes(count))
+        ostream.append(char)
 
     return ostream
 
 
 def rle_decode(istream):
     ostream = []
-    for count, char in np.array(istream).reshape(-1, 2):
+    for count_char in np.array(istream).reshape(-1, 5):
+        count = bytes2int(count_char[:4])
+        char = count_char[4]
         ostream.extend([char] * count)
 
     return np.array(ostream)
+
+
+def int2bytes(u32):
+    return bytearray([
+            (u32 >> 24) & 0xFF,
+            (u32 >> 16) & 0xFF,
+            (u32 >> 8 ) & 0xFF,
+            (u32 >> 0 ) & 0xFF])
+
+
+def bytes2int(b):
+    return (b[0] << 24) + (b[1] << 16) + (b[2] << 8) + b[3]
 
--- rle/test_rle.py
+++ rle/test_rle.py
@@ -1,9 +1,17 @@
-from . import rle_code, rle_decode
+from . import rle_code, rle_decode, int2bytes, bytes2int
 
 
 def test_code_decode():
-    s = bytearray('aaaabccc', encoding='utf-8')
+    s = bytearray('aaaaabccc', encoding='utf-8')
     coded = rle_code(s)
     
     assert bytearray(rle_decode(coded)) == s
+
+
+def test_int2bytes():
+    assert int2bytes(256) == bytearray([0, 0, 1, 0])
+
+
+def test_bytes2int():
+    assert bytes2int([0, 0, 1, 0]) == 256
 
