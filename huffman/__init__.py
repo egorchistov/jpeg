f4062b25da4d399647c4e571e6389cb0218fc2c7
egorchistov <egorchistov@mail.ru>
2021-06-05 19:20:18 +0300

    Refactor and add some docstrings to huffman coding

--- huffman/__init__.py
+++ huffman/__init__.py
@@ -1,9 +1,25 @@
+"""Huffman coding
+
+This module implements huffman coding.
+
+Example
+-------
+>>> from collections import Counter
+>>> import huffman
+>>> string = b"aabc"
+>>> codes = huffman.make_codes(Counter(string))
+>>> length, coded = huffman.code(string, codes)
+>>> decoded = huffman.decode(coded, codes, length)
+"""
+
 from heapq import heappush, heappop, heapify
 
 from bitbuffer import BitBuffer
 
 
 class Node:
+    """Non-leaf tree node."""
+
     def __init__(self, left, right):
         self.parent = None
         left.parent = right.parent = self
@@ -17,25 +33,34 @@ class Node:
         return self.weight < other.weight
 
 
-class Leaf(Node):
+class Leaf:
+    """Tree leaf."""
+
     def __init__(self, symbol, weight):
         self.parent = None
         self.symbol = symbol
         self.weight = weight
 
+    def __lt__(self, other):
+        return self.weight < other.weight
+
     @property
     def code(self):
-        code = ""
+        """Make huffman code for current symbol."""
+
+        huffman_code = ""
         cur = self
         while cur.parent is not None:
             codebit = "0" if cur is cur.parent.left else "1"
-            code = codebit + code
+            huffman_code = codebit + huffman_code
             cur = cur.parent
 
-        return code
+        return huffman_code
 
 
 def preorder(root):
+    """Return all leaves from tree using preorder."""
+
     if isinstance(root, Leaf):
         return [root]
 
@@ -43,6 +68,17 @@ def preorder(root):
 
 
 def make_codes(symbolweights):
+    """Make huffman codes.
+
+    Parameters
+    ----------
+    symbolweights : collections.Counter
+
+    Returns
+    -------
+    codes : dict[str, str]
+    """
+
     leaves = [Leaf(symbol, weight) for symbol, weight in symbolweights.items()]
 
     heapify(leaves)
@@ -57,6 +93,8 @@ def make_codes(symbolweights):
 
 
 def code(string, codes):
+    """Code string using huffman coding."""
+
     bbuf = BitBuffer()
     length = 0
 
@@ -68,19 +106,20 @@ def code(string, codes):
     return length, bbuf.to_bytearray()
 
 
-def decode(bytes_, codes, length):
+def decode(ibytes, codes, length):
+    """Decode string using huffman coding."""
+
     inverse_codes = {v: k for k, v in codes.items()}
-    bbuf = BitBuffer(bytes_)
+    bbuf = BitBuffer(ibytes)
 
     string = bytearray()
 
     while length:
-        code = str(bbuf.pop())
-        length -= 1
-        while not code in inverse_codes.keys():
-            code += str(bbuf.pop())
-            length -= 1
-        char = inverse_codes[code]
+        char_code = str(bbuf.pop())
+        while not char_code in inverse_codes.keys():
+            char_code += str(bbuf.pop())
+        length -= len(char_code)
+        char = inverse_codes[char_code]
 
         string.append(char)
 
