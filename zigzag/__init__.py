1e18197aa2de7e2d590be24c9187e50f11fcbba3
egorchistov <egorchistov@mail.ru>
2021-06-05 19:00:09 +0300

    Add some docstrings to zigzag

--- zigzag/__init__.py
+++ zigzag/__init__.py
@@ -1,7 +1,22 @@
+"""This module implement zigzag scan for 8x8 blocks.
+
+This is useful in JPEG algorithm for reordering DCT coefficients.
+
+Examples
+--------
+>>> import numpy as np
+>>> import zigzag
+>>> coefs = np.array(128).reshape(-1, 8, 8)
+>>> zigzaged = zigzag.scan(coefs)
+>>> zigzag.inverse(zigzaged)
+"""
+
 import numpy as np
 
 
 def scan64(block):
+    """Apply zigzag scan for one 8x8 block."""
+
     indexes = [
             0,   1,  8, 16,  9,  2,  3, 10,
             17, 24, 32, 25, 18, 11,  4,  5,
@@ -16,6 +31,8 @@ def scan64(block):
 
 
 def inverse64(block):
+    """Restore one 8x8 block from zigzaged input."""
+
     indexes = [
              0,  1,  5,  6, 14, 15, 27, 28,
              2,  4,  7, 13, 16, 26, 29, 42,
@@ -30,8 +47,30 @@ def inverse64(block):
 
 
 def scan(blocks):
+    """Apply zigzag scan for all blocks.
+
+    Parameters
+    ----------
+    blocks : np.ndarray of shape (n, 8, 8)
+
+    Returns
+    -------
+    zigzaged : np.ndarray of shape n * 8 * 8
+    """
+
     return np.array([scan64(block) for block in blocks]).flatten()
 
 
 def inverse(blocks):
+    """Restore all blocks from zigzaged input.
+
+    Parameters
+    ----------
+    blocks : np.ndarray of shape n * 8 * 8
+
+    Returns
+    -------
+    blocks : np.ndarray of shape (n, 8, 8)
+    """
+
     return np.array([inverse64(block) for block in blocks.reshape(-1, 64)])
